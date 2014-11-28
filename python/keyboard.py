f5709d47415187a044b65c9fb9ae3fea7e52c8bf
cbott <cbott6@gmail.com>
2014-11-28 20:40:43 +0000

    made keyboard reader control an led

--- python/keyboard.py
+++ python/keyboard.py
@@ -42,27 +42,51 @@ class TermIO:
 
 ####setup code
 gpio.setmode(gpio.BOARD)
-LED_PIN = 22
-gpio.setup(LED_PIN, gpio.OUT)
+RED_PIN = 11
+GREEN_PIN = 12
+BLUE_PIN = 13
+gpio.setup(RED_PIN, gpio.OUT)
+gpio.setup(BLUE_PIN, gpio.OUT)
+gpio.setup(GREEN_PIN, gpio.OUT)
 #Term = TermIO()
 win = curses.initscr()
 win.nodelay(1)
 curses.noecho()
 
+gpio.output(RED_PIN, True)
+gpio.output(GREEN_PIN, True)
+gpio.output(BLUE_PIN, True)
+
 ####main code
 key=" "
-break_key = 'a'#keypress to end the loop
+break_key = ' '#keypress to end the loop
 
 while key!=ord(break_key):
     key = win.getch()#get keyboard input
-    if key!=-1:
-	gpio.output(LED_PIN, True)
+    
+    if key==ord('r'):#turn red on
+	gpio.output(RED_PIN, False)
+	gpio.output(GREEN_PIN, True)
+        gpio.output(BLUE_PIN, True)
+    elif key==ord('g'):
+        gpio.output(RED_PIN, True)
+	gpio.output(GREEN_PIN, False)
+        gpio.output(BLUE_PIN, True)
+    elif key==ord('b'):
+        gpio.output(RED_PIN, True)
+	gpio.output(GREEN_PIN, True)
+        gpio.output(BLUE_PIN, False)
     else:#no keys pressed
-	gpio.output(LED_PIN, False)    
-    time.sleep(0.01)
+	gpio.output(RED_PIN, True)
+	gpio.output(GREEN_PIN, True)
+        gpio.output(BLUE_PIN, True)
+        
+    time.sleep(0.03)
 print(key)
 
 #cleanup procedures
 curses.endwin()
-gpio.output(LED_PIN, False)
+gpio.output(RED_PIN, True)
+gpio.output(GREEN_PIN, True)
+gpio.output(BLUE_PIN, True)
 gpio.cleanup()
