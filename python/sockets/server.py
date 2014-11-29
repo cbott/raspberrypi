e5ac24a4fb8ffd4c2832d0d4eaa5c363b81fdf12
cbott <cbott6@gmail.com>
2014-11-29 12:51:27 -0500

    modified server program to be better

--- python/sockets/server.py
+++ python/sockets/server.py
@@ -5,22 +5,27 @@ HOST = ''                 # Symbolic name meaning the local host
 PORT = 50007              # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
+s.listen(1)
 
-data = "none"
-while data != "halt":
-    s.listen(1)
+while 1:
     conn, addr = s.accept()
     #print ('Connected by', addr)
-    print()
-    
-    data = conn.recv(1024).decode("UTF-8")
-    if not data:
-        break
-    print("Received from client:",data)
-    #d_to_send = "I received '"+data+"' from client"
-    d_to_send = input("Reply: ")
-    conn.send(d_to_send.encode("UTF-8"))
-conn.close()
-print("end")
+    #print()
+    data = "none"
+    while 1:
+        data = conn.recv(1024).decode("UTF-8")
+        if not data:
+            break
+        print("Received from client:",data)
+        #d_to_send = "I received '"+data+"' from client"
+        val = input("Reply: ")
+        d_to_send = val
+        conn.send(d_to_send.encode("UTF-8"))
+        if val=='halt':
+            print("stopping")
+            conn.close()
+            print("Connection closed")
+            break
+
 
 
