9f7ca6a00676a9fdeadb0ddcdc4e1fcb31560fbf
cbott <cbott6@gmail.com>
2014-12-01 18:31:02 -0500

    updated server to send lists

--- python/sockets/server.py
+++ python/sockets/server.py
@@ -1,5 +1,6 @@
 # Echo server program
 import socket
+import random as rand
 
 HOST = ''                 # Symbolic name meaning the local host
 PORT = 50007              # Arbitrary non-privileged port
@@ -10,18 +11,22 @@ s.listen(1)
 while 1:
     conn, addr = s.accept()
     #print ('Connected by', addr)
-    #print()
     data = "none"
     while 1:
         data = conn.recv(1024).decode("UTF-8")
         if not data:
             break
         print("Received from client:",data)
-        #d_to_send = "I received '"+data+"' from client"
-        val = input("Reply: ")
+
+        user = input()
+        #random rgb value
+        val = [rand.randint(0,1),rand.randint(0,1),rand.randint(0,1)]
+        if user == "halt":
+            val = "'halt'"
         d_to_send = val
-        conn.send(d_to_send.encode("UTF-8"))
-        if val=='halt':
+        conn.send(str(d_to_send).encode("UTF-8"))
+        print("sent ",val)
+        if val=="'halt'":
             print("stopping")
             conn.close()
             print("Connection closed")
