de84295b5fe26da878d2f8524ae221bc12ab5fbf
cbott <cbott6@gmail.com>
2014-11-30 22:30:59 -0500

    automated client responses

--- python/sockets/client.py
+++ python/sockets/client.py
@@ -5,17 +5,25 @@ import socket
 HOST = '192.168.1.15'    # The remote host
 PORT = 50007              # The same port as used by the server
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+d_to_send = "Connetction made!"
+s.connect((HOST, PORT))
+s.send(d_to_send.encode("UTF-8"))
+print('Attempted to make connection')
+data = s.recv(1024).decode("UTF-8")
+s.close()
+
 while 1:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
-    d_to_send = raw_input("Data to send: ")
+    data = s.recv(1024).decode("UTF-8")
+    d_to_send = "Pi recieved:"+data
 
+    print(d_to_send)
     s.connect((HOST, PORT))
     s.send(d_to_send.encode("UTF-8"))
-    print('sent "'+d_to_send+'"')
-    data = s.recv(1024).decode("UTF-8")
     s.close()
-    print 'Server says: ', data
+    
     if data == "halt":
         break
     print
--- python/sockets/controller.py
+++ python/sockets/controller.py
@@ -3,9 +3,17 @@
 import pygame, sys
 from pygame.locals import *
 
+size = width, height = (500, 500)
+current_pos = (width/2.0,height/2.0)
+background = (255,255,255)
+
 pygame.init()
-screen = pygame.display.set_mode((200,200))
+screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Pi Communications")
+screen.fill(background)
+
+point = pygame.image.load("point.bmp")
+pointrect = point.get_rect(center = current_pos)
 
 clock = pygame.time.Clock()
 
@@ -21,8 +29,20 @@ while 1:
 ##            if event.key == pygame.K_RIGHT:
 ##                print("right")
 
+    move = [0,0]
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        print("left")
-    if keys[pygame.K_RIGHT]:
-        print("right")
+    if keys[K_LEFT]:
+        move[0] = -1
+    if keys[K_RIGHT]:
+        move[0] = 1
+    if keys[K_UP]:
+        move[1] = -1
+    if keys[K_DOWN]:
+        move[1] = 1
+
+    pointrect = pointrect.move(move)
+
+    screen.blit(point, pointrect)
+
+    pygame.display.flip()
+    
