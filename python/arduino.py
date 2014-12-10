48e7555569e29717e9f3a408f8d34067af7f2c81
cbott <cbott6@gmail.com>
2014-12-10 01:08:49 +0000

    Updated i2c.py with a delay to avoid crashing the connection with the arduino. IT WORKS

--- python/arduino.py
+++ python/arduino.py
@@ -1,17 +1,16 @@
-import time
+import time, random
 from i2c import *
 
 arduino = I2C()
 DATA_BITS = 4#bits used as data. must match value on arduino
 while True:
-	identifier = input("Id (1-15): ")
-	data = input("Data (1-15): ")
-	
-	val = combine_bits(DATA_BITS, identifier, data)#make id the 1st 4 bits
+    identifier = input("Id (1-15): ")
+    data = input("Data (1-15): ")
+    val = combine_bits(DATA_BITS, identifier, data)#make id the 1st 4 bits
                                             #and data last 4 bits and limit to 4 bits
-	arduino.writeNumber(val)
-	print "[RPi] Sent:", val
-	time.sleep(0.01)
-	number = arduino.readNumber()
-	print"[Arduino] Received:", number
-	print
+    arduino.writeNumber(val)
+    print "[RPi] Sent:", val
+    time.sleep(0.01)
+    number = arduino.readNumber()
+    print"[Arduino] Received:", number
+    print
--- python/sockets/i2c.py
+++ python/sockets/i2c.py
@@ -1,12 +1,18 @@
-import smbus
+import smbus, time
 
 class I2C(object):
 	"""An I squared C (I2C) device to communicate with using the RPi"""
 	def __init__(self, address=0x04):
 		self.address = address
 		self.bus = smbus.SMBus(1)
+		#the time required for the slave to evaluate data
+		self.response_time = 0.04
 	def writeNumber(self, value):
 		self.bus.write_byte(self.address, value)
+		#this delay gives time for the device to respond
+		#any value < 0.04 will cause the arduino to freeze up
+		#a value of 0 can be used if readNumber is not called immediately after writeNumber
+		time.sleep(self.response_time)
 		return -1
 	def readNumber(self):
                 rec = 0
@@ -17,7 +23,9 @@ class I2C(object):
                         except IOError:
                                 rec = 0
 		return number
-
+        def setNoDelay(self):
+                """sets the sleep time to 0. Makes writeNumber() non-blocking"""
+                self.response_time = 0
 def combine_bits(databits, num1, num2):
         #take 2 numbers and combine into 1 byte
         #num1 = 10 = 0b00001010 and num2 = 5 = 0b00000101 and databits = 4
